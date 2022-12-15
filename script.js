37681bf14c831ffc25da196cc14153c840f6259d
UEFA1688 <110843342+UEFA1688@users.noreply.github.com>
2022-12-15 08:40:27 +0700

    s

--- script.js
+++ script.js
@@ -1,51 +1,20 @@
-var firebaseConfig = {
-	apiKey: "AIzaSyBvEBRORlJ4-tvuTUHkl7rNeJg1eWzp-w0",
-	authDomain: "arduinorc-a1fcc.firebaseapp.com",
-	databaseURL: "https://arduinorc-a1fcc-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "arduinorc-a1fcc",
-	storageBucket: "arduinorc-a1fcc.appspot.com",
-	messagingSenderId: "353482246377",
-	appId: "1:353482246377:web:ac77b51cd3b812e2254878",
-	measurementId: "G-HGD9TY1F80"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyBvEBRORlJ4-tvuTUHkl7rNeJg1eWzp-w0",
+  authDomain: "arduinorc-a1fcc.firebaseapp.com",
+  databaseURL: "https://arduinorc-a1fcc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "arduinorc-a1fcc",
+  storageBucket: "arduinorc-a1fcc.appspot.com",
+  messagingSenderId: "353482246377",
+  appId: "1:353482246377:web:ac77b51cd3b812e2254878",
+  measurementId: "G-HGD9TY1F80"
+};
+firebase.initializeApp(firebaseConfig);
 
-  const joystickContainer = document.querySelector('#joystick-container');
-  const joystickHandle = document.querySelector('#joystick-handle');
-  const forwardButton = document.querySelector('#forward-button');
-  const backwardButton = document.querySelector('#backward-button');
-  const leftButton = document.querySelector('#left-button');
-  const rightButton = document.querySelector('#right-button');
-  
-  const joystick = new VirtualJoystick({
-	container: joystickContainer,
-	mouseSupport: true,
-  });
-  
-  forwardButton.addEventListener('mousedown', () => {
-	// Move the car forward
-  });
-  
-  backwardButton.addEventListener('mousedown', () => {
-	// Move the car backward
-  });
-  
-  leftButton.addEventListener('mousedown', () => {
-	// Turn the car left
-  });
-  
-  rightButton.addEventListener('mousedown', () => {
-	// Turn the car right
-  });
-  
-  // Update the position of the joystick handle
-  setInterval(() => {
-	joystickHandle.style.top = `${joystick.deltaY() + 150}px`;
-	joystickHandle.style.left = `${joystick.deltaX() + 150}px`;
-  }, 1);
-  
+const database = firebase.database();
 
-
-
-  
+function sendCommand(command) {
+  database.ref("command").set(command);
+}
+function sendCommand(commands) {
+  database.ref("commands").set(commands);
+}
